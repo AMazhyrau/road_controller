ef7b18f8544bd82929ce46b8be65b7651bb49bd9
Andrei Mazhyrau <andreymozhirov@gmail.com>
2017-11-28 18:36:25 +0300

    BasicLaneDetector apply Hough transform

--- src/main.cc
+++ src/main.cc
@@ -8,22 +8,30 @@
 #include "subsystem/perception/detection/lane_detector_strategy.h"
 #include "subsystem/perception/detection/basic_lane_detector.h"
 
+namespace {
+  const char kTestVideo[] = "../test_video/source/solidYellowLeft.mp4";
+}
+
 int main() {
-  cv::VideoCapture cap(0); // open the default camera
-  if(!cap.isOpened()) { // check if we succeeded
-    return -1;
+  cv::VideoCapture stream(kTestVideo); // open test video file
+  if(!stream.isOpened()) { // check if we succeeded
+    return EXIT_FAILURE;
   }
 
-  cv::namedWindow("edges",1);
+  cv::namedWindow("Lane Lines detection", cv::WINDOW_AUTOSIZE);
+
   for(;;)
   {
     cv::Mat frame;
-    cap >> frame; // get a new frame from camera
+    stream >> frame; // get a new frame from camera
+
+    if (frame.empty()) break;
+
     LaneDetectorStrategy<BasicLaneDetector> detector;
     cv::Mat edges = detector.useStrategy(frame);
     cv::imshow("edges", edges);
 
-    if(cv::waitKey(30) >= 0) break;
+    if(cv::waitKey(15) >= 0) break;
   }
 
   return EXIT_SUCCESS;
--- src/subsystem/perception/detection/basic_lane_detector.cc
+++ src/subsystem/perception/detection/basic_lane_detector.cc
@@ -8,9 +8,17 @@ cv::Mat BasicLaneDetector::execute(const cv::Mat& frame) {
   cv::Mat edges;
   cv::cvtColor(frame, edges, cv::COLOR_BGR2GRAY);
   cv::GaussianBlur(edges, edges, cv::Size(7,7), 1.5, 1.5);
-  cv::Canny(edges, edges, 50, 150);
+  cv::Canny(edges, edges, 50, 150, 3);
   cv::dilate(edges, edges, cv::Mat());
 
-  return edges;
+  std::vector<cv::Vec4i> lines;
+  cv::HoughLinesP(edges, lines, 1, CV_PI / 180, 35, 10, 10);
+
+  for (const auto& l : lines) {
+    cv::line(frame, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0, 0, 255), 3, cv::LINE_AA);
+  }
+
+  return frame;
 }
+
 #pragma clang diagnostic pop
